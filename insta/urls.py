ba3007050779e6322e84ff8b46bc50aa1c8834bc
naomi <siguneema@gmail.com>
2019-10-16 09:10:17 +0300

    add CDN links to the head of the file to help in styling

--- insta/urls.py
+++ insta/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+# from django.urls import path, include
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    # url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^search/$',views.search,name='search'),
+    url(r'^post_profile/(?P<pk>\d+)$',views.others_profile,name='others_profile'),
 ]    
  
 if settings.DEBUG:
